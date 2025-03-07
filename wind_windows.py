5eee7c97d409bdb7c910fd5cd14d94aca997b350
lepers <benjamin.lepers@gmail.com>
2025-03-07 22:41:27 +0100

    adding back and front limit

--- wind_windows.py
+++ wind_windows.py
@@ -25,17 +25,25 @@ def perpendicular_vectors(vector):
 # create a function to draw a semi circle joining the two arrow of the perpendicular vectors (pink arrows)
 def semi_circle(vect_perp1, vect_perp2):
     originx, originy = 0, 0
-    constant_radius = 15
+    constant_radius = 20
     #normv1 = np.linalg.norm(vect_perp1)
     #normv2 = np.linalg.norm(vect_perp2)
     
-    alpha  = np.linspace(0, np.pi, 10)
+    alpha  = np.linspace(0, np.pi, 100)
     mx = np.cos(alpha) * vect_perp1[0] - np.sin(alpha) * vect_perp1[1]
     my =  np.sin(alpha) * vect_perp1[0] + np.cos(alpha) * vect_perp1[1]
 
     return mx * constant_radius / np.linalg.norm(vect_perp1), my * constant_radius / np.linalg.norm(vect_perp1)
 
-def plotting(real_wind, boat_vector, ortho1, ortho2, mx, my):
+
+def back_limit(boat_vector):
+    constant_radius = 20
+    normv = np.linalg.norm(boat_vector)
+    #normva = np.linalg.norm(apparent_wind)
+    v  = np.array([boat_vector[1], -boat_vector[0]]) * constant_radius / normv
+    
+    return v
+def plotting(real_wind, boat_vector, ortho1, ortho2, mx, my, back_l, front_l):
 
     xmin, xmax = -20, 20
     ymin, ymax = -20, 20
@@ -47,7 +55,9 @@ def plotting(real_wind, boat_vector, ortho1, ortho2, mx, my):
     plt.quiver(originx, originy, apparent[0], apparent[1], angles='xy', scale_units='xy', scale=1, color='red', label="apparent wind")
     plt.quiver(originx, originy, ortho1[0], ortho1[1], angles='xy', scale_units='xy', scale=1, color='pink')
     plt.quiver(originx, originy, ortho2[0], ortho2[1], angles='xy', scale_units='xy', scale=1, color='pink')
-    plt.plot(mx, my, 'ro')
+    plt.plot(mx, my, 'gray')
+    plt.quiver(originx, originy, back_l[0], back_l[1], angles='xy', scale_units='xy', scale=1, color='purple', label="kite window ")
+    plt.quiver(originx, originy, front_l[0], front_l[1], angles='xy', scale_units='xy', scale=1, color='purple')
     plt.xlim(xmin,xmax)
     plt.ylim(ymin, ymax)
     plt.xlabel('X')
@@ -60,7 +70,7 @@ def plotting(real_wind, boat_vector, ortho1, ortho2, mx, my):
 
 real_wind = vectors(270,10)
 print(real_wind)
-boat_vector = vectors(-45, 10)
+boat_vector = vectors(45, 10)
 print(boat_vector)
 
 app_wind = apparent_wind(real_wind, boat_vector)
@@ -69,5 +79,7 @@ ortho_vect1, ortho_vect2 = perpendicular_vectors(app_wind)
 print(ortho_vect1)
 print(ortho_vect2)
 mx, my = semi_circle(ortho_vect1, ortho_vect2)
-plotting(real_wind, boat_vector, ortho_vect1, ortho_vect2, mx, my)
+back_l = back_limit(boat_vector)
+front_l = ortho_vect2 * 20 / np.linalg.norm(ortho_vect2)
+plotting(real_wind, boat_vector, ortho_vect1, ortho_vect2, mx, my, back_l, front_l)
 semi_circle(ortho_vect1, ortho_vect2)
