c4170364183bd74fe6d5a4cf1c52eae014c1076c
lepers <benjamin.lepers@gmail.com>
2025-03-08 17:08:19 +0100

    adding the fasthtml functinality

--- wind_windows_class.py
+++ wind_windows_class.py
@@ -1,13 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from dataclasses import dataclass
+from fasthtml.common import *
+import decimal
+from decimal import Decimal
+from fh_matplotlib import matplotlib2fasthtml
 
 @dataclass
 class Windcalc:
     real_wind: np.ndarray
     boat_vector: np.ndarray
-    #originx: int = 0
-    #originy: int = 0
     constant_radius: int = 20
 
     @property
@@ -50,11 +52,12 @@ class Drawing:
     front_l: np.ndarray
     originx: int = 0
     originy: int = 0
-    xmin: int = -20
-    xmax: int = 20
-    ymin: int = -20
-    ymax: int = 20
+    xmin: int = -30
+    xmax: int = 30
+    ymin: int = -30
+    ymax: int = 30
     
+    @matplotlib2fasthtml
     def plotting(self):
         plt.quiver(self.originx, self.originy, self.real_wind[0], self.real_wind[1], angles='xy', scale_units='xy', scale=1, color='blue', label="real wind")
         plt.quiver(self.originx, self.originy, self.boat_vector[0], self.boat_vector[1], angles='xy', scale_units='xy', scale=1, color='black', label="trajectory")
@@ -73,7 +76,7 @@ class Drawing:
         plt.legend()
         plt.grid(True)
         plt.gca().set_aspect('equal', adjustable='box')
-        plt.show()
+        #plt.show()
 
 Wind_instance = Windcalc(real_wind=Windcalc.polar_to_cartesian(270, 10),\
                          boat_vector=Windcalc.polar_to_cartesian(45, 10))
@@ -101,3 +104,88 @@ Drawing_instance = Drawing(real_wind=real_wind,
                            back_l=back_l,
                            front_l=front_l)
 Drawing_instance.plotting()
+
+
+app, rt = fast_app()
+
+
+@rt("/info")
+def info():
+    s1 = " Pour dessiner la fenetre de vol on a utilisé du code python, la librairie fasthtml et matplotlib"
+    s2 = " L'utilisateur doit entrer la vitesse du vent réel, la direction de la trajectoire et la vitesse du rider"
+    s3 = " Le vent réel est toujours le vent venant du Nord (haut de la fenetre), cela simplifie le calcul et ne change rien au calcul de la fenetre de vol"
+    s4 = " Une fois determiné le vent apparent, on trace la perpendiculaire à ce vent apparent et le demi cercle associé"
+    s5 = " On trace ensuite deux autres vecteurs, la limite basse de la fenetre de vol et le bord de la fenetre de vol"
+    s6 = " Le secteur entre ces deux lignes est appelé la fenetre de vol utile (en violet sur la figure)"
+    s7 = " Pour plus d'information, voir le code source à mon adresse github"
+    
+     
+    return (
+
+            Div(
+                H1('Informations sur le calcul de la fenetre de vol'),
+                P(s1),
+                P(s2),
+                P(s3),
+                P(s4),
+                P(s5),
+                P(s6),
+                P(s7),
+                P(A("github", href="https://github.com/PythonBen/fenetre_kite")),
+                P(A('retour', href='/')),
+                cls="container",
+                style="padding-top: 10px;"
+                )
+
+           )
+
+@rt("/")
+def index():
+
+    first_form = Form(method="post", action="/submit_form")(
+    Fieldset(
+        Label('Vent réel, vient du Nord (haut du la fenetre)', Input(name='real_wind_speed',placeholder="En noeuds ou km/h, par default vent du Nord",type="number",step="1",min="0",required=True)),
+        Label('Trajectoire', Input(name='trajectory',placeholder="En °, par exemple 45° est la direction NE",type="number",step="5",min="0",required=True,default="45")),
+        Label('Vitesse du rider', Input(name='rider_speed',placeholder="Vitesse en noeuds ou km/h du rider",type="number",step="1",min="1",required=True)),
+            ),
+        Button("Entrez le vent réel, la direction et vitesse du rider", type="submit")
+            )
+    return Titled("Calcul de la fenetre de vol d'un kite",
+        Div(first_form, cls="form-container"),
+        P(A('Plus d\'info', href='/info')))
+    
+@rt("/submit_form")
+def submit(real_wind_speed: float, trajectory: float, rider_speed: float):
+    real_wind_angle = 270
+    try:
+        Wind_instance = Windcalc(real_wind=Windcalc.polar_to_cartesian(real_wind_angle, real_wind_speed),\
+                                 boat_vector=Windcalc.polar_to_cartesian(trajectory, rider_speed))
+        
+        real_wind = Wind_instance.real_wind
+        app_wind = Wind_instance.apparent_wind
+        boat_vector = Wind_instance.boat_vector
+        ortho1, ortho2 = Wind_instance.perpendicular_vectors(app_wind)
+        mx, my = Wind_instance.semi_circle(ortho1)
+        back_l = Wind_instance.back_limit(boat_vector)
+        front_l = ortho2*Wind_instance.constant_radius/np.linalg.norm(ortho2)
+
+        
+        Drawing_instance = Drawing(real_wind=real_wind,
+                                   boat_vector=boat_vector,
+                                   app_wind=app_wind,
+                                   ortho1=ortho1,
+                                   ortho2=ortho2,
+                                   mx=mx,
+                                   my=my,
+                                   back_l=back_l,
+                                   front_l=front_l)
+        #Drawing_instance.plotting()
+        
+    except Exception as e:
+        return Div(P("Error"), P(str(e)))
+    #return Div(P("Success"))
+    return Div(
+        Drawing_instance.plotting(),
+         P(A('Retour', href='/')))
+
+serve()
